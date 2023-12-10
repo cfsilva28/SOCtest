0f42f0cd603c9fb5432a6557f813c7821442c621
Cesar Silva <ceesarf4@gmail.com>
2023-12-10 08:46:19 -0300

    Ajuste dependencias HomeSocPage

--- SOC/src/test/java/Pages/HomeSocPage.java
+++ SOC/src/test/java/Pages/HomeSocPage.java
@@ -5,19 +5,18 @@ import Utils.WebDriverUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class HomeSocPage extends WebDriverUtils {
-    WebDriver driver;
+public class HomeSocPage{
+    private WebDriverUtils webUtils;
 
     public HomeSocPage(WebDriver driver) {
-        super(driver);
-        this.driver = driver;
+        this.webUtils = new WebDriverUtils(driver);
     }
     public HomeSocPage abiriNavegador(String url){
         Util.inicializeWebDriver(url);
         return this;
     }
     public HomeSocPage ClicarLupa() {
-        click(By.cssSelector(".elementor-sticky__spacer .elementor-element-a224482"));
+        webUtils.clicar(By.cssSelector(".elementor-sticky__spacer .elementor-element-a224482"));
         return this;
     }
 }
--- SOC/src/test/java/Utils/WebDriverUtils.java
+++ SOC/src/test/java/Utils/WebDriverUtils.java
@@ -1,12 +1,16 @@
 package Utils;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class WebDriverUtils {
     private WebDriver driver;
@@ -14,34 +18,38 @@ public class WebDriverUtils {
 
     public WebDriverUtils(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver,Duration.ofMillis(1000L));
+        this.wait = new WebDriverWait(this.driver,Duration.ofSeconds(20));
     }
-
-    public void click(By element) {
-        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+    public void clicar(By by) {
+        esperarElementoVisivel(by);
+        driver.findElement(by).click();
     }
-
-    public void typeText(By element, String text) {
-        WebElement webElement = wait.until(ExpectedConditions.visibilityOfElementLocated(element));
-        webElement.clear();
-        webElement.sendKeys(text);
+    public void esperarElementoVisivel(By by){
+        try{
+            wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        } catch (TimeoutException e){
+            Assert.fail("Elemento não encontrado dentro do tempo esperado " + by + ", erro: " + e);
+        }
     }
-
-    public String getText(By element) {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(element)).getText();
-    }
-
-    public void navigateTo(String url) {
-        driver.get(url);
-    }
-
-    public boolean isDisplayed(By element) {
-        try {
-            return wait.until(ExpectedConditions.visibilityOfElementLocated(element)).isDisplayed();
-        } catch (Exception e) {
-            return false;
+    public void esperarWebElementoClicavel(WebElement element){
+        try{
+            wait.until(ExpectedConditions.elementToBeClickable(element));
+        } catch (TimeoutException e) {
+            Assert.fail("Elemento não encontrado dentro do tempo esperado: " + element + ", erro: " + e);
         }
     }
 
-    // Você pode adicionar mais métodos conforme necessário
+    public void esperarWebElementoLocated(By by){
+        try{
+            wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        } catch (TimeoutException e) {
+            Assert.fail(" Elementos não foram encontrados dentro do tempo esperado: " + by + ", erro: " + e);
+        }
+    }
+    public void passarSobreElementoLista(By by, Integer numeroLista){
+        List<WebElement> elements = driver.findElements(by);
+        WebElement element = elements.get(numeroLista);
+        Actions action = new Actions(driver);
+        action.moveToElement(element).moveToElement(element).click().build().perform();
+    }
 }
